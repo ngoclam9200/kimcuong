d3ffbd9c10feebf4368231687126c87ea34b37ea
ngoclam2000 <lam.bui@dotnets.org>
2022-11-08 22:02:30 +0700

    21321

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Bạn gì đó ơi, mình có điều này muốn nói á`,
+    btnIntro: 'OK',
+    title: 'Mình thích bạn á , bạn làm người yêu mình nha 😙',
+    desc: 'bạn không trả lời hoặc thoát ra, tức là bạn thích mình đó nha :> ',
+    btnYes: 'Có, mình thật sự thích bạn 💖💖💖',
+    btnNo: '💔 Không! 💔 ',
+    question:'Nói cho mình biết đi vì sao bạn thích mình 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì mình cũng thích bạn Ahiiiii 😊😊',
+    mess: 'Mình biết mà, mình cũng thích bạn lắm 🥰. Love you 3000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Em nha. 💬💬',
+    btnAccept: 'Ok nuôn 💖',
+    messLink: 'https://www.facebook.com/huynh.kimcuong.16'
+}
